f8476bc2a062c2c438c7350ea15ff41e07aecad9
Matheus-Mannarino <mths.mannarino@gmail.com>
2025-10-27 08:53:27 -0300

    contagem_consertada

--- src/lista_de_paises/src/components/CountryGrid.jsx
+++ src/lista_de_paises/src/components/CountryGrid.jsx
@@ -1,18 +1,21 @@
 // src/components/CountryGrid.jsx
 import CountryCard from './CountryCard';
 
-function CountryGrid({countries}) {
+function CountryGrid({ countries, favorites = [], onToggleFavorite }) {
   return (
     <div className="country-grid">
-      {countries.map(country => (
-      <CountryCard
-        key={country.id}
-        flag={country.flag}
-        name={country.name}
-        capital={country.capital}
-        population={country.population}
-        language={country.language}
-      />
+      {countries.map((country) => (
+        <CountryCard 
+          key={country.id}
+          id={country.id}
+          flag={country.flag}
+          name={country.name}
+          capital={country.capital}
+          population={country.population}
+          language={country.language}
+          isFavorite={favorites.includes(country.id)}
+          onToggleFavorite={onToggleFavorite}
+        />
       ))}
     </div>
   );
